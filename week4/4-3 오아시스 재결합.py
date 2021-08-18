bd68a8d962aa51384c067229b1fc6eaf86e86f6d
scarletYewon <kwn09032@gmail.com>
2021-08-18 18:19:38 +0900

    Create 4-3 오아시스 재결합.py

---
+++ week4/4-3 오아시스 재결합.py
@@ -0,0 +1,20 @@
+n = int(input())
+answer = 0
+stack = []
+
+for i in range(n):
+    h = int(input())
+    while stack and stack[-1][0] < h:
+        answer += stack.pop()[1]
+    if stack and stack[-1][0] == h:
+        cnt = stack.pop()[1]
+        answer += cnt
+        if len(stack) != 0:
+            answer += 1
+        stack.append((h, cnt+1))
+    else:
+        if len(stack) != 0:
+            answer += 1
+        stack.append((h, 1))
+
+print(answer)
